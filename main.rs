a51e843cfe297b4c8873f9b294e0159060f235f9
Eder Costa <eder@encantos.co>
2022-01-30 19:43:57 -0300

    basic operands execution with memory

--- main.rs
+++ main.rs
@@ -1,24 +1,50 @@
+// just print one of the ui texts 
 fn computer_ui(text : i32){
     if text == 1 {
         print!("rust brainfuck interpreter");
         print!("\n\nPlease input branfuck sequence:");
     }
+
+    if text == 2 {
+        println!("UNKNOWN OPERAND!!!");
+    }
 }
 
-
 fn main(){
-   
     computer_ui(1);
-    
-    let memory : [i32; 32] = [0; 32]; // 32 bits of computer memory 
+        
+    let mut memory: [i32; 32] = [0; 32]; // 32 bits of computer memory 
+    let mut pointer: i32 = 0;
 
+    // Creating a mutable string to store a command
     let mut line = String::new();    
+    
+    // reading std in 
     std::io::stdin().read_line(&mut line).unwrap();
 
-    let mut command_array = line.split("");
-    for s in command_array {
+    // Running by each instruction operand
+    let mut operand_array = line.split("");
+    
+    // run by all chars in the code
+    for s in operand_array {
+        
+        if s == ">" {
+            pointer+=1;
+        } else if s == "<" {
+            pointer-=1;
+        } else if s == "+" {
+            memory[pointer as usize]+=1;
+        } else if s == "-" {
+            memory[pointer as usize]-=1;
+        } else if s == "[" {
+            println!("start loop");
+        } else if s == "]" {
+            println!("check finish loop");
+        }
+        
         println!("{}", s);
     }
-    print!("{}", &mut line);
+    
+    // print!("{}", &mut line);
 
 }
--- readme.md
+++ readme.md
@@ -1,3 +1,18 @@
 ## About 
 
 This is a brainfuck rust interpreter
+
+## Dependencies 
+
+- Rust 
+
+## How to compile 
+
+```
+rustc main.rs
+```
+
+
+## running 
+
+open the binary file **main**
